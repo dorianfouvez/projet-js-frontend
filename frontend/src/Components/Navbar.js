862f00b99d2721bb3777b4d22f1282e709cb2d23
Dorian <dorian.fouvez@student.vinci.be>
2020-11-17 15:05:54 +0100

    modif navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,7 +1,7 @@
 let navBar = document.querySelector("#navBar");
 
 const Navbar = (userData) => {
-  let navbarClassName = "navbar navbar-expand-md navbar-dark bg-gradient-dark-purple sticky-top mb-2";
+  let navbarClassName = "navbar navbar-expand-md navbar-dark bg-gradient-dark-purple sticky-top";
   let navbar;
   if (userData) {
     navbar = `
@@ -13,7 +13,7 @@ const Navbar = (userData) => {
       <!-- Collapse body -->
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <!-- Navbar Right Side -->
-        <div class="navbar-nav">
+        <div class="navbar-nav mt-auto">
           <a class="nav-item nav-link" href="#" data-uri="/"><i class="fab fa-rebel text-black"></i> Synopsis</a>
           <a class="nav-item nav-link" href="#" data-uri="/"><i class="fas fa-address-book"></i> Contactez-nous</a>
         </div>
@@ -24,13 +24,13 @@ const Navbar = (userData) => {
         </div>
 
         <!-- Navbar Left Side -->
-        <div class="navbar-nav">   
+        <div class="navbar-nav mt-auto">   
           <a class="nav-item nav-link" href="#" data-uri="/list">List</a>
           <a class="nav-item nav-link" href="#" data-uri="/logout">Logout</a>
           <a class="nav-item nav-link" href="#" data-uri="/profile">${userData.username}</a>
         </div>
       </div>
-    </nav>`;
+    `;
   } else {
     navbar = `
       <!-- Button -->
@@ -41,23 +41,23 @@ const Navbar = (userData) => {
       <!-- Collapse body -->
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <!-- Navbar Right Side -->
-        <div class="navbar-nav">
+        <div class="navbar-nav mt-auto">
           <a class="nav-item nav-link" href="#" data-uri="/"><i class="fab fa-rebel text-black"></i> Synopsis</a>
           <a class="nav-item nav-link" href="#" data-uri="/contact"><i class="fas fa-address-book"></i> Contactez-nous</a>
         </div>
 
         <!-- Centered Logo -->
         <div class="mx-auto">
-          <a class="navbar-brand" href="/game"><img src="src/assets/logo.png" alt="Logo"></a>
+          <a class="navbar-brand" href="/game"><img src="assets/logo-site.png" alt="Logo" id="logo-site"></a>
         </div>
 
         <!-- Navbar Left Side -->
-        <div class="navbar-nav">
+        <div class="navbar-nav mt-auto">
         <a class="nav-item nav-link" href="#" data-uri="/login"><i class="fa fa-fw fa-user text-black"></i> Se connecter</a> 
           <a class="nav-item nav-link" href="#" data-uri="/register"><i class='fas fa-file-signature'></i> S'inscrire</a>
         </div>
       </div>
-    </nav>`;
+    `;
   }
 
   navBar.className = navbarClassName;
