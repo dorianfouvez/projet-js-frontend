db3911381ae4b2cc65623d8940ee618185016b6f
boris fischer <treetsdj@gmail.com>
2020-11-30 14:54:27 +0100

    premieres modifs nouvelle scene

---
+++ frontend/src/Components/phaser/GenreScene.js
@@ -0,0 +1,9 @@
+import Phaser from "phaser";
+
+const PATH_ASSETS = "../assets/";
+const PATH_BUTTON = PATH_ASSETS + "button/";
+const PATH_PROGRESSBAR = PATH_ASSETS + "progressBar/";
+const PATH_UI = PATH_ASSETS + "ui/";
+const PATH_CURSORS = PATH_UI + "cursors/";
+const PATH_GENDERS = PATH_UI + "genders/";
+const PATH_SELECTS = PATH_UI + "selects/";
