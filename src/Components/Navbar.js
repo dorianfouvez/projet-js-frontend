d2969dab7d63872db02915f5cce2815a1b0bcb11
alexandre <alexandre.a.pronce@student.vinci.be>
2020-12-11 14:34:42 +0100

    add admin acces

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ let navBar = document.querySelector("#navBar");
 const Navbar = (userData) => {
   let navbarClassName = "navbar navbar-expand-md navbar-dark bg-gradient-dark-purple sticky-top";
   let navbar;
-  if (userData) {
+  if (userData && userData.isAdmin==true) {
     navbar = `
       <!-- Button -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" >
@@ -31,7 +31,35 @@ const Navbar = (userData) => {
         </div>
       </div>
     `;
-  } else {
+  } else if(userData) {
+    navbar = `
+    <!-- Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapse body -->
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <!-- Navbar Right Side -->
+      <div class="navbar-nav mt-auto">
+        <a class="nav-item nav-link" href="#" data-uri="/"><i class="fab fa-rebel text-black"></i> Synopsis</a>
+        <a class="nav-item nav-link" href="#" data-uri="/contact"><i class="fas fa-address-book"></i> Contactez-nous</a>
+      </div>
+
+      <!-- Centered Logo -->
+      <div class="mx-auto">
+        <a class="navbar-brand" href="#"><img src="assets/logo-site/logo.png" data-uri="/game" alt="Logo" id="logo-site" onmouseover="this.setAttribute('src', 'assets/logo-site/logo-over.png');" onmouseout="this.setAttribute('src', 'assets/logo-site/logo.png');"></a>
+      </div>
+
+      <!-- Navbar Left Side -->
+      <div class="navbar-nav mt-auto">   
+        <a class="nav-item nav-link" href="#" data-uri="/logout">Logout</a>
+        <a class="nav-item nav-link" href="#" data-uri="/profile">${userData.username}</a>
+      </div>
+    </div>
+  `;
+  }
+  else {
     navbar = `
       <!-- Button -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" >
